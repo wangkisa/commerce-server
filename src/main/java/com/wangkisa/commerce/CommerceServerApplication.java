c6ff397963a2a59f04c9390efabdc6efda744272
DESKTOP-D6TCOD1\wgchoi <wangkisa@gmail.com>
2022-12-02 18:07:26 +0900

    first commit

---
+++ src/main/java/com/wangkisa/commerce/CommerceServerApplication.java
@@ -0,0 +1,13 @@
+package com.wangkisa.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangkisa/commerce/CommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangkisa.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
